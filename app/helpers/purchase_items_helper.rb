1eca1480f3a8550f51e8be167ec3f4f2ff4704bb
Mr Jacky Chan <bman917@gmail.com>
2014-02-09 13:05:47 +0800

    caching

--- app/helpers/purchase_items_helper.rb
+++ app/helpers/purchase_items_helper.rb
@@ -1,2 +1,7 @@
 module PurchaseItemsHelper
+	def cache_key_for_purchase_item
+	    count          = PurchaseItem.count
+	    max_updated_at = PurchaseItem.maximum(:updated_at).try(:utc).try(:to_s, :number)
+	    "#{cache_key_for_purchase_order}-purchase_item/all-#{count}-#{max_updated_at}"
+	end
 end
--- app/helpers/purchase_orders_helper.rb
+++ app/helpers/purchase_orders_helper.rb
@@ -1,2 +1,7 @@
 module PurchaseOrdersHelper
+	def cache_key_for_purchase_order
+	    count          = PurchaseOrder.count
+	    max_updated_at = PurchaseOrder.maximum(:updated_at).try(:utc).try(:to_s, :number)
+	    "purchase_order/all-#{count}-#{max_updated_at}"
+	end
 end
